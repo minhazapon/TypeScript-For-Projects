f0d0ee38cd3ae1e39056ba7927c065beab65552f
minhazapon <minhajulabedinapon@gmail.com>
2024-11-07 06:23:47 -0800

    added file

--- problemsolving.ts
+++ problemsolving.ts
@@ -6,22 +6,107 @@
 // ----------------------------------------------
 
     const sumArray = (number: number[]): number =>{
+
+
            const num = number 
+
            const arr = num 
+
            const sumArrayNumIs = arr 
+
            const all = sumArrayNumIs
+
            return all.reduce(( num1, num2 ) => num1 + num2 )
+           
     }
 
     const arrValue = sumArray([1, 2, 3, 4, 5]) 
-    const inValueZ = arrValue + 15 
-    const valueZ = inValueZ - 15
-    console.log(valueZ)
+    const inValueZ = arrValue 
+    console.log(inValueZ)
     
 // ----------------------------------------------
 
   
+////problem --2 
+
+/////////////////////////
+
+
+
+
+////////////////////////////
+
+// -----------------------------------------------------
+
+////problem --3 
+
+/////////////////////////
   
+    const countWordOccurrences = (sentnce: string, wrd: string) =>{
+
+
+         const lowerS = sentnce.toLowerCase() 
+         const lowerWrd = wrd.toLowerCase()  
+
+         const WordZ = lowerS.split(" ")
+
+         let allCount = 0 ;
+
+         for(const WrdZ of WordZ){
+
+            if(WrdZ === lowerWrd){
+                
+                allCount++
+
+            }
+
+         }
+
+         return allCount
+        
+    }
+
+    const allCountWordZ = countWordOccurrences("I love typescript", "typescript") 
+
+    console.log(allCountWordZ)
+
+////////////////////////////
+
+/////---------------------------------------
+  
+////problem --4
+
+/////////////////////////
+
+
+
+
+////////////////////////////
+
+////------------------------
+
+
+////problem --5
+
+/////////////////////////
+
+      
+     const getProperty = <G, P extends keyof G > (object: G , key: P  ): G[P] =>{
+           
+           const property = object
+            
+           return property[key]
+     }
+
+     const Person = { name: "Alice",   age: 23 };
+
+     const result = getProperty(Person, "name")
+
+     console.log(result)
+    
+
+////////////////////////////
+
 
 
 
