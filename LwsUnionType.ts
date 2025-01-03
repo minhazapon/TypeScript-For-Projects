5b7a5d915060c492f04d4c2d77abffc3c74deb57
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-03 05:11:49 -0800

    added file

---
+++ LwsUnionType.ts
@@ -0,0 +1,46 @@
+{
+
+
+    //union type
+
+
+    type value = string | number  
+
+
+
+    type items = {
+
+        i1: value,
+        i2: value,  
+        i3: value,
+        i4: value      
+
+
+    }
+
+    const all: items = {
+
+
+       i1: 'burkha',
+       i2: 'ppunjabi',
+       i3: 'pant',
+       i4: 'genji'
+
+    }
+
+
+    console.log(all.i1, all.i2, all.i3, all.i4)
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+}
