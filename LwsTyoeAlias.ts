2bbc2a3257e450e86e473096f4e9a1e7fb02ce00
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-03 06:09:59 -0800

    added file

---
+++ LwsTyoeAlias.ts
@@ -0,0 +1,26 @@
+{
+
+    
+    type finalValue = string | number
+
+
+    const think = ( a: finalValue, b: finalValue ) =>{
+
+        // const all = a + b 
+
+        // console.log(all)
+
+
+    }
+
+    think(112, 12123)
+
+
+
+
+
+
+
+
+
+}
