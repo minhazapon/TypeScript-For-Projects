b0b4aeb76ec90adc3ec8a4af6a8a0e1f982dd228
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-03 05:21:01 -0800

    added file

---
+++ LwsAny.ts
@@ -0,0 +1,22 @@
+{
+
+     //any type of ts
+
+
+     type resultIs = any  
+     const name: resultIs = 'minhazul abedin apon' 
+     console.log(name) 
+
+     const value: resultIs = 123344
+     const value1: resultIs = 454665
+     const value3: resultIs = true
+     const value4: resultIs = false
+     const value5: resultIs = 'name of me'
+     const value6: resultIs = 1950374409
+     const value7: resultIs = true
+
+     console.log(value, value1, value3, value4, value5, value6, value7)
+
+
+
+}
