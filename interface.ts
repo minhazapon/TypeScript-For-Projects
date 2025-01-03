fcc91a6f6d761d9fc39a0d8e47fe54d441e768e2
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-02 21:10:14 -0800

    added file

--- interface.ts
+++ interface.ts
@@ -30,13 +30,12 @@
 
     /////array in interface//// 
 
-
     interface all {
 
        [index: number] : number
 
     } 
-
+    
     const allArr: all = [ 1, 233, 455, 4667, 456, 3456, 4356, 6778, 45 ] 
 
     console.log(allArr[2], allArr[4], allArr[5], allArr[6], allArr[7])
@@ -54,6 +53,53 @@
     const allF: fun = (num1, num2) => num1 + num2
     const a = allF(20, 40)
     console.log(a)
+
+
+   
+
+    interface bazar{
+     
+       
+        b1: string,
+        b2: string,
+        b3: string,
+        b4: string,
+        allPrice: 2000
+
+    }
+
+
+    const allBazar: bazar = {
+
+       
+        b1:"gazor",
+        b2: "lau",
+        b3: "kodu",
+        b4: 'boroi',
+        allPrice: 2000
+
+
+    } 
+
+
+    console.log(allBazar.b1, allBazar.b2, allBazar.b3, allBazar.b4, allBazar.allPrice) 
+
+
+
+    interface ProductsArray {
+
+      [ index: number ] : number
+
+
+    }
+
+    const PA: ProductsArray = [ 1223, 2344, 345, 3455, 23455, 23 ] 
+
+
+    console.log(PA)
+
+
+
 
 
 
