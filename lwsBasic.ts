1c3353cfb96514031b7cb3dae33458474aaba8e4
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-03 04:47:09 -0800

    added file

---
+++ lwsBasic.ts
@@ -0,0 +1,68 @@
+{
+
+
+    //string 
+
+    const river: string = "podda" 
+
+    console.log(river) 
+
+    const river2: string = 'meghna' 
+
+    console.log(river2)
+    
+
+    //number
+    
+    const nums: number = 123456 
+
+    const nums2: number = 234545
+
+    const nums3: number = 67543 
+
+    const nums4: number = 98090
+
+    console.log(nums, nums2, nums3, nums4)
+
+
+    //boolean
+
+
+    const isAponAmerican : boolean = false
+    
+    const isAponBangladesh: boolean = true 
+
+    console.log(isAponAmerican, isAponBangladesh)
+  
+
+    // array in typeScript 
+
+    const allProducts: string[] = [ 'lau', 'kodu', 'tel', 'saban', 'shampo' ] 
+    console.log(allProducts) 
+
+
+    //array tupleScript 
+    
+    // let books: [string | boolean] = [ 'book name is Apon The Infinite Lover', true ]
+
+    // console.log(books)
+
+    const Infinite: (string | number)[] = [ 'Apon is infinite loved By >>>', 1  ] 
+
+    console.log(Infinite)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
