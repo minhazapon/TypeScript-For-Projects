a7db7ac9c2afd5254732e22d7935f2a89b36dc8d
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-01-03 05:43:02 -0800

    added file

---
+++ LwsFunction.ts
@@ -0,0 +1,47 @@
+{
+
+
+    // function in typeScript
+
+
+    const typeBaby = (x: number, y: number, z: number) =>{
+            
+          const valueZIs = x + y + z 
+
+          const all = valueZIs * 700 
+
+          const ofVlaueIs = all / 200 
+
+          const allIs = ofVlaueIs - 10 
+
+          console.log(allIs)
+
+          if( allIs > 50 ){
+
+            console.log('type is good')
+
+          } else {
+
+            console.log('type is not good')
+
+          }
+ 
+
+    }
+
+
+    typeBaby(23, 24, 232)
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
